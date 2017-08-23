64ad3afc82c8f9e4a114fe79f0bd873a2c08922f
Jason Andersen <andersen.jason@gmail.com>
2017-08-23 16:05:28 -0400

    Initial commit. Blank slate with a class and a test but no implementation of either one.

---
+++ src/main/java/fizzbuzz/FizzBuzz.java
@@ -0,0 +1,16 @@
+package fizzbuzz;
+
+/**
+ * The FizzBuzz service that operates on these rules:
+ *
+ *    - if the input is divisible by 3, then return 'Fizz'
+ *    - if the input is divisible by 5, then return 'Buzz'
+ *    - if the input is divisible by 15, then return 'FizzBuzz'
+ *    - otherwise, return the input
+ */
+public class FizzBuzz {
+
+    public String calculate(int input) {
+        return null;
+    }
+}
---
+++ src/test/java/fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,14 @@
+package fizzbuzz;
+
+import org.junit.Test;
+
+/**
+ * Test the {@link FizzBuzz} class.
+ */
+public class FizzBuzzTest {
+
+    @Test
+    public void myFirstTest() {
+
+    }
+}
